b96bb22de56d58c2c91ac2cff993ee1c51609e83
Sheri Richardson <shrdson@gmail.com>
2017-10-02 21:24:07 +0200

    Display rates on page

--- js/app.js
+++ js/app.js
@@ -9,6 +9,13 @@ const loadData = function() {
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       const data = JSON.parse(xhr.responseText);
+      const usdRate = data.bpi.USD.rate;
+      const gbpRate = data.bpi.GBP.rate;
+      const eurRate = data.bpi.EUR.rate;
+
+      usd.innerHTML = "&dollar; " + usdRate;
+      gbp.innerHTML = "&pound; " + gbpRate;
+      eur.innerHTML = "&euro; " + eurRate;
     }
   };
 
